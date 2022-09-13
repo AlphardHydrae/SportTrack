58399317c14ec4f31e59bad32d45d382441813bf
AlphardHydrae <sophie_bertrand@zohomail.eu>
2022-09-13 10:23:32 +0200

    [UPDATE] added check constraints to db

--- data/create_db.sql
+++ data/create_db.sql
@@ -6,7 +6,13 @@ CREATE TABLE Utilisateur (
     taille INTEGER,
     poids INTEGER,
     email VARCHAR(100) PRIMARY KEY,
-    mdp VARCHAR(50)
+    mdp VARCHAR(50),
+    CONSTRAINT ck_sexe CHECK (
+        sexe = "M"
+        OR sexe = "F"
+    ),
+    CONSTRAINT ck_taille CHECK (taille > 0),
+    CONSTRAINT ck_poids CHECK (poids > 0)
 );
 CREATE TABLE Activite (
     date DATE PRIMARY KEY,
@@ -19,7 +25,13 @@ CREATE TABLE Activite (
     duree TIME,
     distance DOUBLE PRECISION,
     unUtilisateur VARCHAR(100),
-    CONSTRAINT fk_unUtilisateur FOREIGN KEY (unUtilisateur) REFERENCES Utilisateur(email) ON DELETE CASCADE
+    CONSTRAINT fk_unUtilisateur FOREIGN KEY (unUtilisateur) REFERENCES Utilisateur(email) ON DELETE CASCADE,
+    CONSTRAINT ck_fMin CHECK (fMin > 0),
+    CONSTRAINT ck_fMax CHECK (fMax > 0),
+    CONSTRAINT ck_fMoy CHECK (fMoy > 0),
+    CONSTRAINT ck_heure CHECK (hFin > hDebut),
+    CONSTRAINT ck_duree CHECK (duree > "00:00:00"),
+    CONSTRAINT ck_distance CHECK (distance > 0),
 );
 CREATE TABLE Data (
     time TIME PRIMARY KEY,
@@ -29,5 +41,13 @@ CREATE TABLE Data (
     altitude DOUBLE PRECISION,
     uneActivite DATE,
     CONSTRAINT fk_uneActivite FOREIGN KEY (uneActivite) REFERENCES Activite(date) ON DELETE CASCADE,
-    CONSTRAINT ck_freqCardiaque CHECK (freqCardiaque > 0)
+    CONSTRAINT ck_freqCardiaque CHECK (freqCardiaque > 0),
+    CONSTRAINT ck_latitude CHECK (
+        latitude > -90
+        AND latitude < 90
+    ),
+    CONSTRAINT ck_longitude CHECK (
+        longitude > -180
+        AND latitude < 180
+    )
 );
