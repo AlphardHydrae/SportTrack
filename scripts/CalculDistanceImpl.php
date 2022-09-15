1b4c2bf2530ddab1b50fe5bfdadef6dc55c9b89b
Aenigmant <105350808+Aenigmant@users.noreply.github.com>
2022-09-15 08:35:28 +0200

    s

--- scripts/CalculDistanceImpl.php
+++ scripts/CalculDistanceImpl.php
@@ -4,11 +4,21 @@ class CalculDistanceImpl implements CalculDistance {
     require_once("CalculDistance.php");
 
     function calculDistance2PointsGPS(float $lat1, float $long1, float $lat2, float $long2): float {
+        ret = 0;
         $R = 6378.137;
-
-        return $R*acos(sin($lat2)*sin($lat1)+cos($lat2)*cos($lat1)*cos($long2-$long1));
+        if ($lat1!= null && $long1 != null && $lat2 != null && $long2 != null){
+            
+            ret = $R*acos(sin($lat2)*sin($lat1)+cos($lat2)*cos($lat1)*cos($long2-$long1));
+        }
+        return ret;
     }
 
-    function calculDistanceTrajet(Array $parcours): float { return 0; }
+    function calculDistanceTrajet(Array $parcours): float { 
+        ret = 0;
+        if(&parcours != null){
+
+        }
+        return ret; 
+    }
     }
 ?>
