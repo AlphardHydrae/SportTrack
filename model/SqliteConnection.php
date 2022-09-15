d641a53dcf4ef41cf663fc1467afed4669d758e3
AlphardHydrae <sophie_bertrand@zohomail.eu>
2022-09-15 09:49:53 +0200

    [UPDATE] updated db connection

--- model/SqliteConnection.php
+++ model/SqliteConnection.php
@@ -1,14 +1,29 @@
 <?php 
 class SqliteConnection {
-    try {
-        $db = new PDO('mysql:host=localhost;dbname=$dbname', $root, $AlphardPWD);
-        foreach($dbh->query('SELECT * from Utilisateur') as $row) {
-            print_r($row);
+    function getConnection() {
+        try {
+            $db = new PDO('sqlite:../data/sport_track.db');
+            $db -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            $user = 'sophie_bertrand@zohomail.eu';
+
+            $sth = $db -> prepare("SELECT * FROM Activite JOIN Data ON date = uneActivite WHERE unUtilisateur = '" + $user + "';");
+            $sth -> execute();
+            $result = $sth -> fetchAll();
+
+            $table = '</td><td>';
+
+            foreach ($result as $row) {
+                echo "<tbody><td>" . $row['date'] . $table . $row['description'] . $table . $row['fMoy'] . $table . $row['duree'] . $table . $row['duree'] . $table . $row['distance'] . $table . $row['altitude'] . "</td></tbody>";
+            }
+
+            $db = null;
+        } catch (PDOException $e) {
+            print "Error!: " . $e->getMessage() . "<br/>";
+            die();
         }
-        $db = null;
-    } catch (PDOException $e) {
-        print "Error!: " . $e->getMessage() . "<br/>";
-        die();
     }
+
+    getConnection();
 }
 ?>
---
+++ model/sqlite_connection_test.php
@@ -0,0 +1,13 @@
+<table>
+    <thead>
+        <th>Date</th>
+        <th>Description</th>
+        <th>fMoy</th>
+        <th>Duree</th>
+        <th>Distance</th>
+        <th>Altitude</th>
+    </thead>
+<?php
+require_once 'SqliteConnction.php';
+?>
+</table>
