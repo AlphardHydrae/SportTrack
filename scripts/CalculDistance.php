1be54fc0632943bd79bd8aa89cddbbdb839e310a
AlphardHydrae <sophie_bertrand@zohomail.eu>
2022-09-15 08:24:41 +0200

    [NEW] calculDistance2PointGPS

--- scripts/CalculDistance.php
+++ scripts/CalculDistance.php
@@ -21,9 +21,12 @@ interface CalculDistance {
 
 class CalculDistanceImpl implements CalculDistance {
 
-    function calculDistance2PointsGPS(float $lat1, float $long1, float $lat2, float $long2): float { return 0; }
+    function calculDistance2PointsGPS(float $lat1, float $long1, float $lat2, float $long2): float {
+        $R = 6378.137;
+
+        return $R*acos(sin($lat2)*sin($lat1)+cos($lat2)*cos($lat1)*cos($long2-$long1));
+    }
 
     function calculDistanceTrajet(Array $parcours): float { return 0; }
-    
 }
 ?>
