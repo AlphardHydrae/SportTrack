838373583e0845ca8f0a9fe034c95c8eab6633e1
AlphardHydrae <sophie_bertrand@zohomail.eu>
2022-09-22 12:39:53 +0200

    [NEW] tests insert/delete

--- model/sqlite_connection_test.php
+++ model/sqlite_connection_test.php
@@ -1,9 +1,42 @@
 <?php
+require_once 'Utilisateur.php';
 require_once 'UserDAO.php';
 require_once 'ActivityDAO.php';
 require_once 'ActivityEntryDAO.php';
 
 echo '<h2>User</h2>';
+
+echo '<h3>findAll</h3>';
+$result = UtilisateurDAO::getInstance() -> findAll();
+var_dump($result);
+
+echo '<h3>insert</h3>';
+$n = 'User';
+$p = 'user';
+$d = '2003-06-20';
+$s = 'F';
+$t = '165';
+$pd = '58';
+$e = 'User-user@gmail.com';
+$m = 'user';
+$user = new Utilisateur();
+$user -> init($n, $p, $d, $s, $t, $pd, $e, $m);
+$result = UtilisateurDAO::getInstance() -> insert($user);
+$result = UtilisateurDAO::getInstance() -> findAll();
+var_dump($result);
+
+echo '<h3>delete</h3>';
+$n = 'User';
+$p = 'user';
+$d = '2003-06-20';
+$s = 'F';
+$t = '165';
+$pd = '58';
+$e = 'User-user@gmail.com';
+$m = 'user';
+$user = new Utilisateur();
+$user -> init($n, $p, $d, $s, $t, $pd, $e, $m);
+$result = UtilisateurDAO::getInstance() -> delete($user);
 $result = UtilisateurDAO::getInstance() -> findAll();
 var_dump($result);
 
